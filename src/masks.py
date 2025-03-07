d7298cfd8d02b635d57bf884a141c49367778cfe
grigorii goncharov <grigoriy85@gmail.com>
2025-03-07 19:38:24 +0300

    lesson_9.2_homework

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
-def get_mask_card_number(num_cart: int) -> str:
+def get_mask_card_number(num_cart: str) -> str:
     """Функция принимает на вход номер карты и возвращает ее маску."""
-    if len(str(num_cart)) != 16 or not str(num_cart).isdigit():
-        raise ValueError("Номер карты должен состоять из 16 цифр!")
+    if len(num_cart) != 16 or not num_cart.isdigit():
+        return "Номер карты должен состоять из 16 цифр!"
 
     string = str(num_cart)
     masked_card = f"{string[:4]} {string[4:6]}** **** {string[-4:]}"
@@ -9,7 +9,7 @@ def get_mask_card_number(num_cart: int) -> str:
     return masked_card
 
 
-def get_mask_account(account_number: int) -> str:
+def get_mask_account(account_number: str) -> str:
     """Функция принимает на вход н принимает на вход номер счета и возвращает его маску."""
     if len(str(account_number)) != 20 or not str(account_number).isdigit():
         raise ValueError("Номер счета должен состоять из 20 цифр!")
@@ -20,8 +20,8 @@ def get_mask_account(account_number: int) -> str:
     return masked_account_number
 
 
-card_exemple = 7000792289606341
-account_exemple = 73654108430135874305
+card_exemple = "7000792289606341"
+account_exemple = "73654108430135874305"
 
 print(get_mask_card_number(card_exemple))
 print(get_mask_account(account_exemple))
---
+++ src/widget.py
@@ -0,0 +1,25 @@
+from masks import get_mask_card_number
+
+
+def mask_account_card(string: str) -> str:
+    """Функция маскировки счета и номера карты."""
+    string_split = string.split()
+    name_card = " ".join(string_split[:-1])
+    number_card = string_split[-1]
+
+    return name_card, number_card
+
+
+def get_date(string: str) -> str:
+    """Функция вывода даты"""
+
+    return f"{string[8:10]}.{string[5:7]}.{string[0:4]}"
+
+
+test = "Visa Platinum 8990922113665229"
+name_card, number_card = mask_account_card(test)
+masked_number = get_mask_card_number(number_card)
+print(f"{name_card}: {masked_number}")
+
+test = "2024-03-11T02:26:18.671407"
+print(get_date(test))
