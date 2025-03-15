d1dec9b65c35d1233022a29ea42f69556025e291
grigorii goncharov <grigoriy85@gmail.com>
2025-03-15 13:40:42 +0300

    Hot_fixes

--- src/processing.py
+++ src/processing.py
@@ -1,15 +1,15 @@
-def filter_by_state(list_dict: list, state: str = "EXECUTED") -> list:
+def filter_by_state(list_dict: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция фильтрует данные по указанному параметру"""
 
     new_list = []
     for element in list_dict:
         if element["state"] == state:
             new_list.append(element)
-
     return new_list
 
 
-def sort_by_date(list_dict: list, reverse: bool = True) -> list:
-    """Функция сортирует список словарей на основе ключа date"""
+def sort_by_date(list_dict: list[dict], value_sort: bool = True) -> list[dict]:
+    """Функция сортирует список словарей о дате"""
 
-    return sorted(list_dict, key=lambda x: ["date"], reverse=reverse)
+    sort_list = sorted(list_dict, key=lambda x: x["date"], reverse=not value_sort)
+    return sort_list
