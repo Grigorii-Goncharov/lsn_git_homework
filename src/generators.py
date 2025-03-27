53213ce78cef70ba84c23bc08863b79bbf0bc902
grigorii goncharov <grigoriy85@gmail.com>
2025-03-27 17:53:15 +0300

    def transaction_descriptions

--- src/generators.py
+++ src/generators.py
@@ -16,3 +16,21 @@ for _ in range(2):
     except StopIteration:
         print("Больше нет данных")
         break
+
+def transaction_descriptions(transactions: list[dict[str, Any]]) -> Iterator[str]:
+    """Функция для генератора списков, которая принимает на вход список словарей и возвращает описание каждой
+    операции по очереди"""
+    for transaction in transactions:
+        description = transaction.get("description")
+        if description is not None:
+            yield description
+
+transactions_two: list[dict[str, Any]] = []
+descriptions = transaction_descriptions(transactions_two)
+
+for _ in range(5):
+    try:
+        print(next(descriptions))
+    except StopIteration:
+        print("Больше нет данных")
+        break
